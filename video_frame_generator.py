57318ee9869bb15a1c42601b00fa9b94b3c76b96
Ricky <r.zhang10825@gmail.com>
2025-06-05 20:11:19 -0600

    made code to find contrast values by frame number and determine  flicker counts. Flicker counts in a sliding window range has also been implemented. Next step is to establish a threshold that flicker counts need to exceed to determine if a video can cause photosenstive epipelspy.

--- video_frame_generator.py
+++ video_frame_generator.py
@@ -2,6 +2,8 @@
 
 import cv2
 from helper_function import luminance
+from collections import deque
+import matplotlib.pyplot as plt
 
 
 capture = cv2.VideoCapture(r"C:\Users\PC\Downloads\Pokemon EP38 - Electric Soldier Porygon + ENG Subtitles - Trim.mp4") # tehcnically an array of numpyndarray (frames)
@@ -9,8 +11,10 @@ capture = cv2.VideoCapture(r"C:\Users\PC\Downloads\Pokemon EP38 - Electric Soldi
 f = 0
 last_frame_luminance = -1
 flicker_count = 0
-luminance_array = []
+max_flicker_count = 0
+luminance_queue = deque()
 
+contrast_values = []
 lp = 0 # left pointer for sliding window
 rp = 0 # right pointer for sliding window
 
@@ -52,15 +56,38 @@ while(capture.isOpened()):
     # given relative_luminance, we compare with the last frame to determine if there was a flicker at the current frame. This is if contrast is above 3:1 threshold. 
 
     # case if we have max sliding window size
+    
+    
     if rp - lp >= sliding_window_size:
+        contrast_ratio = max(relative_luminance, last_frame_luminance) / min(relative_luminance, last_frame_luminance)
+        print(f, contrast_ratio)
 
+        if (contrast_ratio >= 3.0):
+            flicker_count += 1
+            max_flicker_count = max(flicker_count, max_flicker_count)
+        last_frame_luminance = relative_luminance
+        luminance_queue.append(relative_luminance)
+        rp += 1
 
+        contrast_values.append(contrast_ratio)
+
+        # check if the removed item forms a flicker 
+
+        removed_luminance = luminance_queue.popleft()
+
+        contrast_ratio = max(removed_luminance, luminance_queue[0]) / min(removed_luminance, luminance_queue[0]) # check to see if the removed item has a flicker with its neighbor
+
+        if (contrast_ratio >= 3.0):
+            flicker_count -= 1
+        
+        lp += 1
         
 
     # case if window size is not maxed out
     else: 
         if last_frame_luminance == -1: # this is the first frame analyzed so don't worry
             last_frame_luminance = relative_luminance
+            luminance_queue.append(relative_luminance)
             rp += 1
             continue
         contrast_ratio = max(relative_luminance, last_frame_luminance) / min(relative_luminance, last_frame_luminance)
@@ -68,13 +95,27 @@ while(capture.isOpened()):
 
         if (contrast_ratio >= 3.0):
             flicker_count += 1
+            max_flicker_count = max(flicker_count, max_flicker_count)
+        contrast_values.append(contrast_ratio)
+        last_frame_luminance = relative_luminance
+        luminance_queue.append(relative_luminance)
         rp += 1
+    f += 1
 
 
-    last_frame_luminance = relative_luminance
+    
+print(max_flicker_count)
     
 
+plt.plot([x for x in range(len(contrast_values))], contrast_values)
+
+plt.xlabel('Frame Number')
+plt.ylabel('Contrast Values')
 
 
 
 
+plt.title('Contrast values in Episode')
+
+# function to show the plot
+plt.show()
